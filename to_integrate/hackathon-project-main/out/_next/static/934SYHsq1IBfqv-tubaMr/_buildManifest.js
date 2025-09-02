506e27bd353acda97cc6fd823c2898b758877f6b
andersonproescholdbell <jjfkou@gmail.com>
2025-09-01 23:01:13 -0400

    ported ai game and other changes

--- to_integrate/hackathon-project-main/out/_next/static/934SYHsq1IBfqv-tubaMr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-580712bba2f524d9.js"],"/_error":["static/chunks/pages/_error-cb181bc066ffb5d9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
